2acc4efacd581fe35ca4758b3677511d6cffe585
TiffanyK17 <yhs@12452-student.yhs.local>
2015-09-14 14:40:21 -0700

    Version 1.1

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 public class Test
 {
-  public static void main (String[] args)
-  {
-    System.out.println ("An Emergency Broadcast");
-  }
+   public static void main(String[] args)
+   {
+      System.out.println("An Emergency Test");
+      System.out.println("This is only a test!");
+      System.out.println("This is a very exclusive class");
+   }
 }
